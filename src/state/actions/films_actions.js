37c2c658a56053c3dcf3dadd18cbcada5b4c4459
Yasser Batas <yasser.batas@gmail.com>
2019-03-31 14:42:41 -0600

    add actions in order to search films

--- src/state/actions/films_actions.js
+++ src/state/actions/films_actions.js
@@ -1,4 +1,4 @@
-import { SET_FILMS_LIST } from '../types/index';
+import { SET_FILMS_LIST, SET_MATCHED_FILMS, IS_LOADING } from '../types/index';
 import Films from '../../services/Films';
 
 export const getFilms = () => {
@@ -6,13 +6,35 @@ export const getFilms = () => {
     try {
       const response = await Films.getFilms();
       dispatch(setFilmsList(response.data));
+      dispatch(isLoading(false));
     } catch (error) {
       console.log(error);
     }
-  }
-}
+  };
+};
 
 export const setFilmsList = filmsList => ({
   type: SET_FILMS_LIST,
   payload: filmsList
-})
+});
+
+export const searchFilm = (filmsList, inputValue) => {
+  return async dispatch => {
+    dispatch(isLoading(true));
+    const matchedFilms = filmsList.filter(film =>
+      film.title.toLocaleLowerCase().includes(inputValue)
+    );
+
+    dispatch(setMatchedFilms(matchedFilms));
+  };
+};
+
+export const setMatchedFilms = matchedFilmsList => ({
+  type: SET_MATCHED_FILMS,
+  payload: matchedFilmsList
+});
+
+export const isLoading = state => ({
+  type: IS_LOADING,
+  payload: state
+});
