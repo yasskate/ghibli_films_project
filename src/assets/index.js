71ea82fef6151544306b6c8408abb31cdbdb32bf
Yasser Batas <yasser.batas@gmail.com>
2019-04-07 06:06:57 -0500

    add assents folder

---
+++ src/assets/index.js
@@ -0,0 +1,5 @@
+export const images = {
+  star: require('./images/star_icon.png'),
+  star_solid_icon: require('./images/star_solid_icon.png'),
+  star_half_icon: require('./images/star_half_icon.png')
+};
