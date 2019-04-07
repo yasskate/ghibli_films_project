fafddd0fc18dcfe416a9c0c1e1e0a72635998aa6
Yasser Batas <yasser.batas@gmail.com>
2019-04-07 05:21:50 -0500

    add small refactor to Loader method

--- src/components/loader/Loader.js
+++ src/components/loader/Loader.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './Loader.css';
 
-export const Loader = () => {
-  return (
-    <div className="loader-container">
-      <img
-        src="https://i.imgur.com/h5PA99R.gif"
-        alt="Totoro is loading the data..."
-      />
-      <h1>Loading...</h1>
-    </div>
-  );
-};
+export const Loader = () => (
+  <div className="loader-container">
+    <img
+      src="https://i.imgur.com/h5PA99R.gif"
+      alt="Totoro is loading the data..."
+    />
+    <h1>Loading...</h1>
+  </div>
+);
