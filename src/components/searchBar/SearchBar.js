0ffe3597adc40baafdb7142c26ea2a01af284592
Yasser Batas <yasser.batas@gmail.com>
2019-04-07 05:23:23 -0500

    add onChange method on searchBar component
    this change was made in order to search coincidences with the value that the user input

--- src/components/searchBar/SearchBar.js
+++ src/components/searchBar/SearchBar.js
@@ -1,36 +1,56 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+import { searchFilm } from '../../state/actions/index';
 
-export const SearchBar = () => {
-  return (
-    <section className="section">
-      <div className="columns is-marginless">
-        <div className="column is-half is-offset-3 is-multiline">
-          <div className="control has-icons-left has-icons-right">
-            <input
-              className="input is-large"
-              type="email"
-              placeholder="Search Films"
-            />
-            <span className="icon is-medium is-left">
-              <i className="fas fa-search" />
-            </span>
-            <span className="icon is-medium is-right">
-              <i className="fas fa-film" />
-            </span>
+class SearchBar extends PureComponent {
+  state = {
+    inputValue: ''
+  };
+
+  onChange = event => {
+    this.setState({ inputValue: event.target.value }, () => {
+      const { filmsList } = this.props;
+      const { inputValue } = this.state;
+
+      this.props.searchFilm(filmsList, inputValue.toLocaleLowerCase());
+    });
+  };
+
+  render = () => {
+    return (
+      <section className="section">
+        <div className="columns is-marginless">
+          <div className="column is-half is-offset-3 is-multiline">
+            <div className="control has-icons-left has-icons-right">
+              <input
+                className="input is-large is-rounded is-primary"
+                type="email"
+                placeholder="Search Films"
+                value={this.state.inputValue}
+                onChange={this.onChange}
+              />
+              <span className="icon is-medium is-left">
+                <i className="fas fa-search" />
+              </span>
+              <span className="icon is-medium is-right">
+                <i className="fas fa-film" />
+              </span>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
-  );
+      </section>
+    );
+  };
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    searchFilm: (filmsList, inputValue) =>
+      dispatch(searchFilm(filmsList, inputValue))
+  };
 };
 
-/* 
-  * input value on search bar
-  * get value from the input
-  * dispatch an action SEARCH_FILM
-  * filter value between filmsList
-  * create a new state filteredFilmsList=[]
-  * dispatch action SET_MATCHED_FILMS if there are matched films
-  * save filteredFilmsList if SET_MATCHED_FILMS was dispatched
-  * render filteredFilmsList instead of FilmsList
-*/
+export default connect(
+  null,
+  mapDispatchToProps
+)(SearchBar);
