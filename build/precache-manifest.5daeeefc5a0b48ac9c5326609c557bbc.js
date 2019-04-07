0612f91f035daff149e5a7dbdbf0005886e7f6b1
Yasser Batas <yasser.batas@gmail.com>
2019-04-07 05:44:44 -0500

    add build folder

---
+++ build/precache-manifest.5daeeefc5a0b48ac9c5326609c557bbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c8e30ebb9c8865fca04d",
+    "url": "/static/js/main.9d107b2c.chunk.js"
+  },
+  {
+    "revision": "6e005176783fa95bd695",
+    "url": "/static/js/2.c3d51022.chunk.js"
+  },
+  {
+    "revision": "c8e30ebb9c8865fca04d",
+    "url": "/static/css/main.50783a58.chunk.css"
+  },
+  {
+    "revision": "6e005176783fa95bd695",
+    "url": "/static/css/2.a164a997.chunk.css"
+  },
+  {
+    "revision": "220dd661e77e8aa57406035f1769032f",
+    "url": "/index.html"
+  }
+];
