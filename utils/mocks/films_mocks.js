422269dc9eabfbd7532477b335546c53499bccf6
Yasser Batas <yasser.batas@gmail.com>
2019-05-11 05:57:16 -0500

    add films mock file

---
+++ utils/mocks/films_mocks.js
@@ -0,0 +1,50 @@
+export const filmsListMockResponse = [
+  {
+    id: '2baf70d1-42bb-4437-b551-e5fed5a87abe',
+    title: 'Castle in the Sky',
+    description:
+      "The orphan Sheeta inherited a mysterious crystal that links her to the mythical sky-kingdom of Laputa. With the help of resourceful Pazu and a rollicking band of sky pirates, she makes her way to the ruins of the once-great civilization. Sheeta and Pazu must outwit the evil Muska, who plans to use Laputa's science to make himself ruler of the world.",
+    director: 'Hayao Miyazaki',
+    producer: 'Isao Takahata',
+    release_date: '1986',
+    rt_score: '95',
+    url:
+      'https://ghibliapi.herokuapp.com/films/2baf70d1-42bb-4437-b551-e5fed5a87abe'
+  },
+  {
+    id: '12cfb892-aac0-4c5b-94af-521852e46d6a',
+    title: 'Grave of the Fireflies',
+    description:
+      'In the latter part of World War II, a boy and his sister, orphaned when their mother is killed in the firebombing of Tokyo, are left to survive on their own in what remains of civilian life in Japan. The plot follows this boy and his sister as they do their best to survive in the Japanese countryside, battling hunger, prejudice, and pride in their own quiet, personal battle.',
+    director: 'Isao Takahata',
+    producer: 'Toru Hara',
+    release_date: '1988',
+    rt_score: '97',
+    url:
+      'https://ghibliapi.herokuapp.com/films/12cfb892-aac0-4c5b-94af-521852e46d6a'
+  },
+  {
+    id: '58611129-2dbc-4a81-a72f-77ddfc1b1b49',
+    title: 'My Neighbor Totoro',
+    description:
+      'Two sisters move to the country with their father in order to be closer to their hospitalized mother, and discover the surrounding trees are inhabited by Totoros, magical spirits of the forest. When the youngest runs away from home, the older sister seeks help from the spirits to find her.',
+    director: 'Hayao Miyazaki',
+    producer: 'Hayao Miyazaki',
+    release_date: '1988',
+    rt_score: '93',
+    url:
+      'https://ghibliapi.herokuapp.com/films/58611129-2dbc-4a81-a72f-77ddfc1b1b49'
+  },
+  {
+    id: 'dc2e6bd1-8156-4886-adff-b39e6043af0c',
+    title: 'Spirited Away',
+    description:
+      'Spirited Away is an Oscar winning Japanese animated film about a ten year old girl who wanders away from her parents along a path that leads to a world ruled by strange and unusual monster-like animals. Her parents have been changed into pigs along with others inside a bathhouse full of these creatures. Will she ever see the world how it once was?',
+    director: 'Hayao Miyazaki',
+    producer: 'Toshio Suzuki',
+    release_date: '2001',
+    rt_score: '97',
+    url:
+      'https://ghibliapi.herokuapp.com/films/dc2e6bd1-8156-4886-adff-b39e6043af0c'
+  }
+];
