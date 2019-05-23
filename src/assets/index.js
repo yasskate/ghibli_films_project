ef5d86e22b1232e1ea3efc1485d492207c11337d
Yasser Batas <yasser.batas@gmail.com>
2019-05-22 23:12:18 -0500

    add loader gif

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,6 @@
 export const images = {
   star: require('./images/star_icon.png'),
   star_solid_icon: require('./images/star_solid_icon.png'),
-  star_half_icon: require('./images/star_half_icon.png')
+  star_half_icon: require('./images/star_half_icon.png'),
+  loader: require('./images/loader.gif')
 };
