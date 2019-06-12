dac03d34bf0de081247a960231a9186529a2df67
Yasser Batas <yasser.batas@gmail.com>
2019-06-11 19:49:48 -0500

    add GoogleImages API instance

--- src/services/Films.js
+++ src/services/Films.js
@@ -1,9 +1,19 @@
 import axios from 'axios';
+import GoogleImages from 'google-images';
+
+const client = new GoogleImages(
+  '004566832713711953075:56aoigtc7gk',
+  'AIzaSyBFvsBrfV7UC34FgjYpoveslL9j6p3Gg8I'
+);
 
 const BASE_URL = 'https://ghibliapi.herokuapp.com';
 
 export default {
   getFilms: async () => {
     return axios.get(`${BASE_URL}/films`);
+  },
+
+  getFilmsCoverImages: async title => {
+    return client.search(`${title} movie poster`, { size: 'large' });
   }
-}
+};
